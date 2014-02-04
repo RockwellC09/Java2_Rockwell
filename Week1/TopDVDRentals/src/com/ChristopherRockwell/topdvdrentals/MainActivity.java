1df0ac26fb85dedbb88a7bd9f781910a2c837525
RockwellC09 <RockwellC09@gmail.com>
2014-02-04 12:22:35 -0600

    Added TopRentalsService class to project
    Currently not functionall

--- Week1/TopDVDRentals/src/com/ChristopherRockwell/topdvdrentals/MainActivity.java
+++ Week1/TopDVDRentals/src/com/ChristopherRockwell/topdvdrentals/MainActivity.java
@@ -1,11 +1,24 @@
+/*
+ * Project: TopDVDRentals
+ * Package: com.ChristopherRockwell.topdvdrentals
+ * @author	Christopher Rockwell
+ * Date: 	Feb 4, 2014
+ */
 package com.ChristopherRockwell.topdvdrentals;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MainActivity.
+ */
 public class MainActivity extends Activity {
 
+    /* (non-Javadoc)
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,6 +26,9 @@ public class MainActivity extends Activity {
     }
 
 
+    /* (non-Javadoc)
+     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ Week1/TopDVDRentals/src/com/ChristopherRockwell/topdvdrentals/TopRentalsService.java
@@ -0,0 +1,27 @@
+/*
+ *@author	Christopher Rockwell 
+ *Description: This service retrieves the top DVD rental from the Rotten Tomatoes API for use 
+ *within the application.
+ */
+package com.ChristopherRockwell.topdvdrentals;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * The Class TopRentalsService.
+ */
+public class TopRentalsService extends IntentService {
+
+	public TopRentalsService() {
+		super(" TopRentalsService");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
